113fa1029a121d62261283beeb0f20072179098c
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-07-07 19:09:39 -0600

    Expensive Sequence done and tested. Changed to i=7 value to 10496705, though

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,27 @@
 # Your code here
-
+# Dictionary for storing values
+storage = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    # # Your code here
+
+    # # This should be done with recursion
+    # # Establish base case where x <= 0
+    if x <= 0:
+        return y+z
+
+    # Check if value of x is a key in the dictionary, return value if so
+    elif (100000000000000*x+y+z) in storage:
+        return storage[(100000000000000*x+y+z)]
+
+    # Recurse function and save keys and values as you go
+    else:
+        n = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        storage[(100000000000000*x+y+z)] = n
+        return n
+
+    # print(x)
+
 
 
 
