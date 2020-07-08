32e9bac66852cfc0e30fb720cc18df89b46ad357
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-07-08 14:55:07 -0600

    Word count done and tested

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,30 @@
+# Use regex to clean up non-alphanumeric characters
+import re
+
 def word_count(s):
-    # Your code here
+    # Dictionary to store counts
+    counts = {}
+
+    # Lowercase everything and replace returns and tabs with spaces
+    s = s.lower().replace('\n', ' ').replace('\t', ' ').replace('\r', ' ')
+
+    # DON'T remove (because it might be escaping an apostrophe)
+    keepers = "\ '"
+    # Get rid of non-useful characters
+    s = re.sub(r'[^\w'+keepers+']', '', s)
+
+    # Split string on whitespace
+    s = s.split()
+
+    # Iterate over each word and either add to counts dictionary or increment
+    for word in s:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+    # Return counts dictionary
+    return counts
 
 
 
