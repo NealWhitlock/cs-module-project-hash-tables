84a9d60ec32434bed29c0291bd351fa9c60596f7
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-07-08 16:09:36 -0600

    No duplicates done and tested. Operates in O(n) time

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,27 @@
 def no_dups(s):
-    # Your code here
+    # Reference dictionary for words as they are encountered
+    ref = {}
 
+    # New string to return
+    new_s = ""
+
+    # Split the input string into words
+    split = s.split()
+
+    # Iterate through words in split string
+    for word in split:
+        # If word is not in reference dictionary add it to new string
+        # with a space and put it in reference dictionary
+        if word not in ref:
+            new_s += word
+            new_s += ' '
+            ref[word] = 1
+        # If the word is in the reference dictionary, don't add it
+        else:
+            continue
+    
+    # Return the new string, but stripped of outside whitespace
+    return new_s.strip()
 
 
 if __name__ == "__main__":
