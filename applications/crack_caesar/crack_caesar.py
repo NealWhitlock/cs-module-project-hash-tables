8309d9c6a0f2de28a1f07d9f143fae2558612f65
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-07-09 16:08:16 -0600

    Crack Caesar done

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,53 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
+# Read in text
+with open("ciphertext.txt") as f:
+    text = f.read()
 
+# Dictionary to count occurances of letters
+counts = {}
+
+# Iterate through characters in text
+# If the character is a letter incremement count in dictionary
+for char in text:
+    if char.isalpha() and char != 'â':
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1
+
+
+# Organize results by most highest count
+sorted_list = list(counts.items())
+# sorted_list.sort(key=lambda pair: (-pair[1],pair[0]))
+sorted_list.sort(key=lambda pair: -pair[1])
+
+# for tup in sorted_list:
+#     print(tup)
+
+# print(len(sorted_list))
+
+# String of alphabet ordered by frequency but doubled
+freq = "ETAOHNRISDLWUGFBMYCPKVQJXZ"
+
+
+# Dictionary to contain key for decoding
+key = {}
+for i, tup in enumerate(sorted_list):
+    key[tup[0]] = freq[i]
+
+# String to hold decrypted text
+decoded = ""
+
+# Reverse mapping to decode text
+for char in text:
+    if char.isalpha() and char != 'â':
+        char = key[char]
+
+    # print(char, end="")
+
+    decoded += char
+
+
+print(decoded)
